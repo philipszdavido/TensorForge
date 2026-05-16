99d8bab2d3ad1d26a4b8e71a29e3058b0f1e2110
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-05-16 12:04:43 +0100

    feat: added sum and mul functions

---
+++ src/math/vector/avg.ts
@@ -0,0 +1,9 @@
+import { Vector } from "../../core/Vector";
+
+export default function avg(vec: Vector) {
+  return vec.avg();
+}
+
+// export default function avg(vec: Array<number>) {
+//   return vec.avg();
+// }
---
+++ src/math/vector/sum.ts
@@ -0,0 +1,5 @@
+import { Vector } from "../../core/Vector";
+
+export default function sum(vec: Vector) {
+  return vec.sum();
+}
