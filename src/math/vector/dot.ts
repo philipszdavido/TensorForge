8728487228565b3a0e90d1462a8799e4a7e81130
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-05-16 15:32:07 +0100

    feat: added dot impl

---
+++ src/math/vector/dot.ts
@@ -0,0 +1,15 @@
+import assert from "../../assert/assert";
+import { Vector } from "../../core/Vector";
+
+// multiply Vectors
+export default function dot(a: Vector, b: Vector): number {
+  assert(a.length == b.length);
+  
+  let sum = 0;
+  for (let index = 0; index < a.length; index++) {
+    const a1 = a.get(index);
+    const b1 = b.get(index);
+    sum += a1 * b1;
+  }
+  return sum;
+}
