252c5ec3580cb74773f03db5e8b4c37a64f27ec1
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-05-17 16:23:52 +0100

    fix: fixed distance bug

--- src/models/KNN.ts
+++ src/models/KNN.ts
@@ -16,7 +16,14 @@ export interface DataLabel {
 }
 
 export default class KNN {
-  constructor(public readonly samples: DataLabel[]) {}
+  constructor(
+    public readonly samples: DataLabel[],
+    public readonly k?: number,
+  ) {
+    if (!k) {
+      k = Math.sqrt(samples.length);
+    }
+  }
 
   euclidean_distance(x1: number[], x2: number[]) {
     // Implementation for calculating distance between two points
@@ -30,7 +37,9 @@ export default class KNN {
 
   predict(data: number[]) {
     const distances = [];
-    let nearestLabel
+    let kNearestNeighbors = [];
+    let pred;
+
     for (const sample of this.samples) {
       // calculate distance to every point
       const d = this.euclidean_distance(sample.data, data);
@@ -40,11 +49,48 @@ export default class KNN {
       distances.sort((a, b) => a.distance - b.distance);
 
       // pick K nearest neighbors
-      nearestLabel=distances[0].label
+      kNearestNeighbors = distances.slice(0, this.k);
 
       // majority vote
-
+      console.log("=====================");
+      
+      pred = this.majorityVote(kNearestNeighbors);
+      //console.log(d, pred, kNearestNeighbors);
+      console.log("=====================")
     }
-    return nearestLabel
+    return pred;
+  }
+
+  //   Think of it like asking neighbors
+  // If you're moving into a neighborhood and ask:
+  // 1 person → opinion may be unreliable
+  // 5 people → better
+  // 100 people → too broad, may lose local relevance
+  // That’s exactly what k does in KNN.
+  majorityVote(data: { distance: number; label: string }[]): string {
+    const votes: Record<string, number> = {};
+
+    for (let i = 0; i < data.length; i++) {
+      const element = data[i];
+
+      const previousVote = votes[element.label] || 0;
+
+      votes[element.label] = element.distance + previousVote;
+    }
+    let highestVotes = 0;
+    let keyWithHighestVotes = "";
+    for (const key in votes) {
+      if (!Object.hasOwn(votes, key)) continue;
+
+      const numVotes = votes[key];
+
+      if (highestVotes < numVotes) {
+        highestVotes = numVotes;
+        keyWithHighestVotes = key;
+      }
+    }
+console.log(data, votes, keyWithHighestVotes);
+
+    return keyWithHighestVotes;
   }
 }
