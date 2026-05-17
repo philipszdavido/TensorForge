79f81f2a79e65257074c0c9ee3444eb8946b8814
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-05-17 17:56:53 +0100

    fix: fixed logic

--- src/models/LogisticRegression.ts
+++ src/models/LogisticRegression.ts
@@ -1,59 +1,45 @@
+import { Vector } from "../core/Vector";
+import error from "../error/error";
+import sigmoid from "../math/sigmoid";
+import dot from "../math/vector/dot";
+
 export default class LogisticExpression {
-  sigmoid(x: number) {
-    // y = 1 / (1 + e^-x)
-    return 1 / (1 + Math.exp(-x));
+
+  predict(x: number[], weights: number[], bias: number) {
+    const result = dot(Vector.from(x), Vector.from(weights));
+    return result + bias;
   }
 
-  step(prob: number) {
-    return prob >= 0.5 ? 1 : 0;
-  }
-
-  calcError(predicted: number, actual: number) {
-    return actual - predicted;
-  }
-
-  predict(w1: number, x1: number, w2: number, x2: number, bias: number) {
-    // z = w1 * width + w2 * length + bias
-    // pred = step(z)
-
-    // ay + bx + c = 0
-    // y = -(b/a)x + -(c/a)
-
-    return w1 * x1 + w2 * x2 + bias;
-  }
-
-  train(features: number[]) {
+  train(features: number[][], label: number[]) {
     let bias = -1;
     // we want to predict if bug is ladybird
 
     // width contributes more to a bug being a ladybird
-    let widthWeight = 0.9;
-
     // length contributes less
-    let lengthWeight = 0.1;
+
     let learningRate = 0.2;
+    let weights = new Array(features[0].length).fill(0)
 
     const epochs = 100;
 
     for (let i = 0; i < epochs; i++) {
       features.forEach((feat, i) => {
+
         const y_bar = this.predict(
-          widthWeight,
-          feat.width,
-          lengthWeight,
-          feat.length,
+          feat,
+          weights,
           bias,
         );
-        const predicted = this.sigmoid(y_bar);
-        const error = this.calcError(predicted, feat.actual);
 
-        widthWeight += learningRate * error * feat.width;
-        lengthWeight += learningRate * error * feat.length;
-        bias += learningRate * error;
+        const predicted = sigmoid(y_bar);
+        const _error = error(predicted, label[i]);
 
-        console.log(feat, y_bar, predicted, error, widthWeight, lengthWeight);
+        weights = weights.map((weight) => learningRate * _error * weight);
+        bias += learningRate * _error;
       });
-      console.log("====================");
     }
   }
 }
+
+// features = [[1,3], [4,2]]
+// label    = [ 0,    1]
