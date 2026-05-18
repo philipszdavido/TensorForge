c0947f1c16a35ec1e8ad6f78c8e352d24a69ca35
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-05-18 14:07:33 +0100

    feat: added BCELoss

---
+++ src/error/BCELoss.ts
@@ -0,0 +1,7 @@
+export default function BCELoss(yHat: number, y: number) {
+  const eps = 1e-15;
+
+  yHat = Math.max(eps, Math.min(1 - eps, yHat));
+
+  return -(y * Math.log(yHat) + (1 - y) * Math.log(1 - yHat));
+}
