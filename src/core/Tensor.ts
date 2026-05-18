524ab92cb55e27d90ea7eb693e88e6e4bb718334
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-05-18 14:09:55 +0100

    fix: added features for Tensor

--- src/core/Tensor.ts
+++ src/core/Tensor.ts
@@ -1,3 +1,11 @@
-class Tensor {
+export class Tensor {
+  constructor(public data: number[]) {}
 
+  static zeros(size: number) {
+    return new Tensor(new Array(size).fill(0));
+  }
+
+  clone() {
+    return new Tensor([...this.data]);
+  }
 }
