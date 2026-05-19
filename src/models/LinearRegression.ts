0f22a2f5f2fb7aed2c278c66d389d2408ed1b718
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-05-19 16:52:08 +0100

    fix: fixed gradient explosion

--- src/models/LinearRegression.ts
+++ src/models/LinearRegression.ts
@@ -8,9 +8,9 @@ export class LinearRegression extends Model {
   constructor(
     public readonly features: number[][],
     public readonly labels: number[],
-    protected epochs: number = 1000,
+    protected epochs: number = 10,
     protected bias: number = -1,
-    protected learningRate: number = 0.2,
+    protected learningRate: number = 0.0001,
   ) {
     super(features, labels, epochs, bias, learningRate);
   }
@@ -18,19 +18,18 @@ export class LinearRegression extends Model {
   private adjustWeights(error: number, features: number[]) {
     this.setWeights(
       this.weights.map(
-        (weight, i) => weight + this.learningRate * error * features[i],
+        (weight, i) => weight - this.learningRate * error * features[i],
       ),
     );
   }
 
   private adjustBias(error: number) {
-    const bias = this.bias + this.learningRate * error;
+    const bias = this.bias - this.learningRate * error;
     this.setBias(bias);
   }
 
   protected calc(x: number[], weights: number[], bias: number) {
-    const result = dot(Vector.from(x), Vector.from(weights));
-    return result + bias;
+    return dot(Vector.from(x), Vector.from(weights)) + bias;
   }
 
   setLearningRate(l: number) {
@@ -59,13 +58,37 @@ export class LinearRegression extends Model {
     return this.activate(this.calc(x, this.weights, this.bias));
   }
 
+  normalizeFeatures() {
+    const numFeatures = this.features[0].length;
+    const numSamples = this.features.length;
+
+    for (let j = 0; j < numFeatures; j++) {
+      let sum = 0;
+      for (let i = 0; i < numSamples; i++) {
+        sum += this.features[i][j];
+      }
+      const mean = sum / numSamples;
+
+      let varianceSum = 0;
+      for (let i = 0; i < numSamples; i++) {
+        varianceSum += Math.pow(this.features[i][j] - mean, 2);
+      }
+      const stdDev = Math.sqrt(varianceSum / numSamples);
+
+      for (let i = 0; i < numSamples; i++) {
+        this.features[i][j] =
+          stdDev === 0 ? 0 : (this.features[i][j] - mean) / stdDev;
+      }
+    }
+  }
+
   train() {
     for (let i = 0; i < this.epochs; i++) {
       this.features.forEach((feat, i) => {
         const y = this.calc(feat, this.weights, this.bias);
 
         const predicted = this.activate(y);
-        const errorValue = error(this.labels[i], predicted);
+        const errorValue = error(predicted, this.labels[i]);
 
         this.adjustWeights(errorValue, feat);
 
