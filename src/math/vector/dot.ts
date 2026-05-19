8e2b518b32c5ae49dbd5a5c35364a68f18d458cd
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-05-19 16:52:23 +0100

    fix: fixed Infinity bug

--- src/math/vector/dot.ts
+++ src/math/vector/dot.ts
@@ -4,11 +4,15 @@ import { Vector } from "../../core/Vector";
 // multiply Vectors
 export default function dot(a: Vector, b: Vector): number {
   assert(a.length == b.length);
-  
+
   let sum = 0;
   for (let index = 0; index < a.length; index++) {
     const a1 = a.get(index);
     const b1 = b.get(index);
+    if (index == 0) {
+      sum = a1 * b1;
+      continue;
+    }
     sum += a1 * b1;
   }
   return sum;
