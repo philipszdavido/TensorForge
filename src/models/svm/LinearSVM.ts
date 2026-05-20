e409cf59ebf6cfeda58febbebe62979a28cbbda5
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-05-20 17:50:12 +0100

    feat: implemented linear svm

---
+++ src/models/svm/LinearSVM.ts
@@ -0,0 +1,43 @@
+import { Vector } from "../../core/Vector";
+import dot from "../../math/vector/dot";
+import { LinearRegression } from "../LinearRegression";
+import LogisticRegression from "../LogisticRegression";
+
+export default class LinearSVM extends LogisticRegression {
+  // regularization strength
+  private readonly C: number = 1.0;
+  calcMargin(y_i: number, data: number) {
+    return y_i * data;
+  }
+
+  train() {
+    for (let i = 0; i < this.epochs; i++) {
+      this.features.forEach((feat, i) => {
+        const z = this.calc(feat, this.weights, this.bias);
+
+        const margin = this.calcMargin(this.labels[i], z);
+
+        if (margin >= 1) {
+          // w = w - lr * (2 * w)
+          this.setWeights(
+            this.weights.map(
+              (weight, i) => weight - this.learningRate * (2 * weight),
+            ),
+          );
+        } else {
+          //  w = w - lr * (2 * w - C * y_i * x_i);
+          //  b = b + lr * C * y_i;
+          this.setWeights(
+            this.weights.map(
+              (weight, i) =>
+                weight -
+                this.learningRate *
+                  (2 * weight - this.C * this.labels[i] * feat[i]),
+            ),
+          );
+          this.setBias(this.bias + this.learningRate * this.C * this.labels[i]);
+        }
+      });
+    }
+  }
+}
