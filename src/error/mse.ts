5f73bcaf725e9c8a5e38b972bc1cb8997cf0bbc0
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-05-21 01:43:16 +0100

    feat: implemented logic for mse

--- src/error/mse.ts
+++ src/error/mse.ts
@@ -1,2 +1,15 @@
 // Mean Square Error
-export default function meanSquareError() {}
+import assert from "../assert/assert";
+
+export default function meanSquareError(x: number[], y: number[]) {
+
+    assert(x.length == y.length);
+
+    let sum = 0
+    for (let i = 0; i < x.length; i++) {
+        const result = (x[i] - y[i]) ** 2
+        sum += result
+    }
+
+    return sum / x.length;
+}
