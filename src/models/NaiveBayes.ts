d6e9ecdd4c726a3d512b09ae67359234b4557437
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-05-26 12:33:26 +0100

    feat: added custom tokenizer

--- src/models/NaiveBayes.ts
+++ src/models/NaiveBayes.ts
@@ -1,103 +1,113 @@
 export interface DataSet {
-  data: string;
-  label: string;
+    data: string;
+    label: string;
 }
 
 export default class NaiveBayes {
-  private TotalOfAllWordsInLabel: Record<string, number> = {};
-  private TotalOfAWordPerLabel: Record<string, Record<string, number>> = {};
-  private TotalCountOfLabels: Record<string, number> = {};
+    private TotalOfAllWordsInLabel: Record<string, number> = {};
+    private TotalOfAWordPerLabel: Record<string, Record<string, number>> = {};
+    private TotalCountOfLabels: Record<string, number> = {};
 
-  constructor(
-    private readonly samples: DataSet[],
-    private readonly labels: string[],
-  ) {}
+    constructor(
+        private readonly samples: DataSet[],
+        private readonly labels: string[],
+    ) {
+    }
 
-  train() {
-    const labels = this.labels;
-    const samples = this.samples;
+    train() {
+        const labels = this.labels;
+        const samples = this.samples;
 
-    for (let i = 0; i < samples.length; i++) {
-      const sample = samples[i];
-      const label = sample.label;
+        for (let i = 0; i < samples.length; i++) {
+            const sample = samples[i];
+            const label = sample.label;
 
-      if (!this.TotalOfAllWordsInLabel[label]) {
-        this.TotalOfAllWordsInLabel[label] = 0;
-      }
+            if (!this.TotalOfAllWordsInLabel[label]) {
+                this.TotalOfAllWordsInLabel[label] = 0;
+            }
 
-      if (!this.TotalCountOfLabels[label]) {
-        this.TotalCountOfLabels[label] = 0;
-      }
-      this.TotalCountOfLabels[label]++;
+            if (!this.TotalCountOfLabels[label]) {
+                this.TotalCountOfLabels[label] = 0;
+            }
+            this.TotalCountOfLabels[label]++;
 
-      const words = sample.data.split(" ");
+            const words = this.tokenize(sample.data);
 
-      for (let j = 0; j < words.length; j++) {
-        const word = words[j];
+            for (let j = 0; j < words.length; j++) {
+                const word = words[j];
 
-        if (!this.TotalOfAWordPerLabel[word]) {
-          this.TotalOfAWordPerLabel[word] = {};
-          for (const label of labels) {
-            this.TotalOfAWordPerLabel[word][label] = 0;
-          }
+                if (!this.TotalOfAWordPerLabel[word]) {
+                    this.TotalOfAWordPerLabel[word] = {};
+                    for (const label of labels) {
+                        this.TotalOfAWordPerLabel[word][label] = 0;
+                    }
+                }
+
+                this.TotalOfAWordPerLabel[word][label] += 1;
+
+                this.TotalOfAllWordsInLabel[label] += 1;
+            }
+        }
+    }
+
+    predict(sampleText: string) {
+        const text = sampleText.split(" ");
+        const labels = this.labels;
+
+        const scores: Record<string, Array<number>> = {};
+        for (const label of labels) {
+            scores[label] = [];
         }
 
-        this.TotalOfAWordPerLabel[word][label] += 1;
+        for (const word of text) {
+            // calc prob of this word for all labels
 
-        this.TotalOfAllWordsInLabel[label] += 1;
-      }
-    }
-  }
+            for (const label of labels) {
+                // prob of this word in class label
 
-  predict(sampleText: string) {
-    const text = sampleText.split(" ");
-    const labels = this.labels;
+                const wordProbInLabel =
+                    (this.TotalOfAWordPerLabel?.[word]?.[label] || 0) /
+                    this.TotalOfAllWordsInLabel[label];
 
-    const scores: Record<string, Array<number>> = {};
-    for (const label of labels) {
-      scores[label] = [];
-    }
+                let TotalOfWordsNotInThisLabel = 0;
+                let TotalOfAWordPerNotInThisLabel = 0;
 
-    for (const word of text) {
-      // calc prob of this word for all labels
+                for (const _label of labels) {
+                    if (_label == label) continue;
+                    TotalOfWordsNotInThisLabel += this.TotalOfAllWordsInLabel[_label];
+                    TotalOfAWordPerNotInThisLabel +=
+                        this.TotalOfAWordPerLabel?.[word]?.[_label] || 0;
+                }
 
-      for (const label of labels) {
-        // prob of this word in class label
+                const otherP =
+                    TotalOfAWordPerNotInThisLabel / TotalOfWordsNotInThisLabel;
 
-        const wordProbInLabel =
-          (this.TotalOfAWordPerLabel?.[word]?.[label] || 0) /
-          this.TotalOfAllWordsInLabel[label];
-
-        let TotalOfWordsNotInThisLabel = 0;
-        let TotalOfAWordPerNotInThisLabel = 0;
-
-        for (const _label of labels) {
-          if (_label == label) continue;
-          TotalOfWordsNotInThisLabel += this.TotalOfAllWordsInLabel[_label];
-          TotalOfAWordPerNotInThisLabel +=
-            this.TotalOfAWordPerLabel?.[word]?.[_label] || 0;
+                scores[label].push(wordProbInLabel / (wordProbInLabel + otherP) || 0);
+            }
         }
 
-        const otherP =
-          TotalOfAWordPerNotInThisLabel / TotalOfWordsNotInThisLabel;
+        const TotalScores: Record<string, number> = {};
+        let highestScore = 0;
+        let highest = "";
+        for (const label of labels) {
+            const Total = scores[label].reduce((acc, val) => acc + val, 0);
+            TotalScores[label] = Total;
 
-        scores[label].push(wordProbInLabel / (wordProbInLabel + otherP) || 0);
-      }
+            if (Total > highestScore) {
+                highestScore = Total;
+                highest = label;
+            }
+        }
+
+        return highest;
     }
 
-    const TotalScores: Record<string, number> = {};
-    let highestScore = 0;
-    let highest = "";
-    for (const label of labels) {
-      const Total = scores[label].reduce((acc, val) => acc + val, 0);
-      TotalScores[label] = Total;
-
-      if (Total > highestScore) {
-        highestScore = Total;
-        highest = label;
-      }
+    tokenize(text: string) {
+        return text
+            .toLowerCase()
+            .replace(/[^a-z0-9\s]/g, "")
+            .split(/\s+/)
+            .filter(Boolean);
     }
 
-    return highest;
-  }
 }
