8fc0b780842e181413a850656eb3d1c01b37add9
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-05-28 21:23:10 +0100

    feat: added neural network implementation

--- src/models/NeuralNetwork.ts
+++ src/models/NeuralNetwork.ts
@@ -1,10 +1,39 @@
-// set input size
-// set hidden layers number and size
-// set output size
+import {Matrix} from "../core/Matrix";
+import {Vector} from "../core/Vector";
+
+type Layer = {};
+
 export default class NeuralNetwork {
-constructor(
-    private readonly inputSize: number,
-    private readonly outputSize: number,
-    private readonly hiddenLayers: number[],) {
+
+    layers: Matrix[] = []
+
+    constructor(public readonly inputSize: number, public readonly hidden: number[], public readonly outputSize: number) {
+
+        let currentInputs = inputSize;
+
+        for (let i = 0; i < hidden.length; i++) {
+
+            const layer = hidden[i]
+            // outputs x inputs
+            this.layers.push(new Matrix(currentInputs, layer));
+            currentInputs = layer;
+        }
+
+        this.layers.push(new Matrix(currentInputs, outputSize));
+
+    }
+
+    forward(input: number[]) {
+
+        let acc: Vector = Vector.from(input);
+
+        for (let i = 0; i < this.layers.length; i++) {
+            const layer = this.layers[i];
+            acc = Matrix.matrixMulVector(layer, acc);
+        }
+
+        return acc
+
+    }
 }
-}
+
