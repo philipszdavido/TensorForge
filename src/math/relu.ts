7a96b4816d3208b729e520e3752f457417a8abd5
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-05-29 00:00:10 +0100

    feat: added ReLU

--- src/math/relu.ts
+++ src/math/relu.ts
@@ -1,3 +1,17 @@
+import {Vector} from "../core/Vector";
+
 export default function relu(x: number) {
     return Math.max(0, x)
+}
+
+export function ReLU(vec: Vector) {
+
+    const v = new Vector(vec.length);
+
+    for (let i = 0; i < vec.length; i++) {
+        v.set(i, relu(vec.get(i)));
+    }
+
+    return v;
+
 }
